5eafb95a608c486aaf306eaea4a807b4156f8279
RaghRoog <szumon612@interia.pl>
2025-07-16 23:17:44 +0200

    start

---
+++ src/test/java/com/example/zadanie_rekrutacyjne/ZadanieRekrutacyjneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_rekrutacyjne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieRekrutacyjneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
